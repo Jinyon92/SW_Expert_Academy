c4facb5a8ed00c25e02c2500e82cf897992143a0
choijinhyun <chjh0902@naver.com>
2019-05-06 16:37:13 +0900

    화물을 옮기는 자(100개중 95개 정답, 5개 시간초과)

---
+++ sw_expert_academy7508/sw_expert_academy7508/main.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int lefting[50];
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    cin>>T;
+    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        int n;
+        cin>>n;
+        vector<int> lift(n);
+        for(int i=0; i<n; i++){
+            cin>>lift[i];
+        }
+        int m;
+        cin>>m;
+        vector<int> weight(m);
+        for(int i=0; i<m; i++){
+            cin>>weight[i];
+        }
+        sort(weight.begin(), weight.end());
+        sort(lift.begin(), lift.end());
+        int idx = 0;
+        for(int i=0; i<m;){
+            if(lift[idx] >= weight[i]){
+                lefting[idx]++;
+                i++;
+            }else{
+                if(idx < n-1){
+                    idx++;
+                }else{
+                    break;
+                }
+            }
+        }
+        bool flag = true;
+        if(lift[n-1] < weight[m-1]){
+            flag = false;
+        }
+        int ans = 0;
+        while(flag){
+            bool find = false;
+            for(int i=n-1; i>=0; i--){
+                if(lefting[i]){
+                    lefting[i]--;
+                    find = true;
+                }else{
+                    for(int j=i-1; j>=0; j--){
+                        if(lefting[j]){
+                            lefting[j]--;
+                            find = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(!find) break;
+            ans++;
+        }
+        if(flag) cout<<"#"<<test_case<<" "<<ans<<"\n";
+        else cout<<"#"<<test_case<<" "<<-1<<"\n";
+    }
+    return 0;
+}
+
+
